d45f0a49769709ca950f3fb9d02d51347384ba94
EliteCRM <EliteCRM01@gmail.com>
2018-07-30 16:10:57 +0800

    demo 首页增加服务地址输入框

--- RyWebChat/ViewController.m
+++ RyWebChat/ViewController.m
@@ -18,7 +18,7 @@
 #import "MAMessageUtils.h"
 
 //#define MACLIENTSERVERADDR @"https://cht.1919.cn/EliteWebChat"
-#define MACLIENTSERVERADDR @"http://dev.elitecrm.com/webchat" //服务器地址
+//#define MACLIENTSERVERADDR @"http://dev.elitecrm.com/webchat" //服务器地址
 //#define MACLIENTSERVERADDR @"http://192.168.2.80:8980/webchat" //服务器地址
 
 @interface ViewController ()
@@ -26,6 +26,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *userId;
 @property (weak, nonatomic) IBOutlet UITextField *headUrl;
 @property (weak, nonatomic) IBOutlet UITextField *queueId;
+@property (weak, nonatomic) IBOutlet UITextField *serverAddr;
 
 @property (strong, nonatomic) MARyChatViewController *chatViewController;
 
@@ -49,11 +50,13 @@
     NSString *U_Id = [ user objectForKey:@"userId"];
     NSString *H_Url = [ user objectForKey:@"headUrl"];
     NSString *Q_Id = [ user objectForKey:@"queueId"];
+    NSString *Q_ServerAddr = [ user objectForKey:@"serverAddr"];
     
     self.userName.text = U_Name;
     self.userId.text = U_Id;
     self.headUrl.text = H_Url?H_Url:@"https://avatars3.githubusercontent.com/u/15497804?v=3&u=fe6dfc22f6ae32639af26a096f8f67f65b892a28&s=400";
     self.queueId.text = Q_Id?Q_Id:@"1";
+    self.serverAddr.text = Q_ServerAddr?Q_ServerAddr:@"http://dev.elitecrm.com/webchat";
     
 }
 - (IBAction)webChatPressed:(id)sender {
@@ -61,6 +64,7 @@
     [self.userId resignFirstResponder];
     [self.headUrl resignFirstResponder];
     [self.queueId resignFirstResponder];
+    [self.serverAddr resignFirstResponder];
     
     if([self.userName.text isEqualToString:@""] ||
        [self.userId.text isEqualToString:@""]) {
@@ -76,11 +80,15 @@
     if([q_id isEqualToString:@""]){
         q_id = @"1";
     }
+    NSString *q_serverAddr = self.serverAddr.text;
+    if([q_serverAddr isEqualToString:@""]){
+        q_serverAddr = @"http://dev.elitecrm.com/webchat";
+    }
     int parseQueueId = [q_id intValue];
     
     __weak typeof(self) myself = self;
     // NSString *chatTargetId = [[MAChat getInstance] getChatTargetId];
-    [[MAEliteChat shareEliteChat] initAndStart:MACLIENTSERVERADDR userId:self.userId.text name:self.userName.text portraitUri:h_uri chatTargetId:@"1919" queueId:parseQueueId ngsAddr:nil complete:^(BOOL result) {
+    [[MAEliteChat shareEliteChat] initAndStart:q_serverAddr userId:self.userId.text name:self.userName.text portraitUri:h_uri chatTargetId:@"1919" queueId:parseQueueId ngsAddr:nil complete:^(BOOL result) {
         if (result) {
             [myself switchChatViewController];
             
@@ -89,6 +97,7 @@
             [user setObject:self.userId.text forKey:@"userId"];
             [user setObject:self.headUrl.text forKey:@"headUrl"];
             [user setObject:self.queueId.text forKey:@"queueId"];
+            [user setObject:self.serverAddr.text forKey:@"serverAddr"];
         }
         else NSLog(@"初始化或启动失败");
     }];
